484d71eebf2a5b5fa865bcc8d446bc4799940b23
mir-src <264200162+mir-src@users.noreply.github.com>
2026-05-16 08:58:37 +0300

    Implemented simple boards page, add to board to board list flow

--- app.py
+++ app.py
@@ -1,15 +1,24 @@
-from flask import Flask
+from flask import Flask, render_template, request  
+import sqlite3
 
 app = Flask(__name__)
 
+boards = []
+
 @app.route("/")
 def home():
-    return "Welcome to pinboard"
+    return render_template('index.html')
+
+@app.route("/boards", methods=["GET", "POST"])
+def boards_page(): 
+    if request.method == "POST":
+        new_board = request.form["board_name"]
+        boards.append(new_board)
+
+    print(request.method)
+
+    return render_template('boards.html', boards=boards)
 
 if __name__ == "__main__" :
     app.run(debug=True)
-
-
-
-
 
